8c938036ae98b068921e0dbd3908ca99de211559
YELLANKI THARUN <121236907+tharunyellanki@users.noreply.github.com>
2023-03-03 22:11:39 +0530

    Update and rename CSE 310 WEEK1-DAY 5 QUESTION-1 to CSE 310 WEEK1-DAY5-Q1.java

--- CSE 310 WEEK1-DAY5-Q1.java
+++ CSE 310 WEEK1-DAY5-Q1.java
@@ -1,7 +1,7 @@
-1ST QUESTION
+/*1ST QUESTION
 
 Consider that there is a cricket player named Smith who hits certain number of 4s and 6s in a cricket match. Find out total numbers of scores he makes with his 
-4s & 6s only.
+4s & 6s only.*/
 
 import java.io.*;
 import java.util.*;
