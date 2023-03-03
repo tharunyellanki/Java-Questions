ee4012caf56f3b7a47b2c0d43586332b229ed872
YELLANKI THARUN <121236907+tharunyellanki@users.noreply.github.com>
2023-03-03 22:19:27 +0530

    Update and rename CSE 310 WEEK1-DAY 1 to CSE 310 WEEK1-DAY1-Q1.java

--- CSE 310 WEEK1-DAY1-Q1.java
+++ CSE 310 WEEK1-DAY1-Q1.java
@@ -1,6 +1,6 @@
-1 QUESTION
+/*1 QUESTION
 Write a program to take the 2 inputs of integer type from the user. Display the values separated by symbol ‘-’ if the values are positive otherwise display the message “Invalid Input”.
-// CODE FOR THE QUESTION
+// CODE FOR THE QUESTION*/
 
 import java.util.Scanner;
 
@@ -19,20 +19,4 @@ public class Main {
 }
 }
 
-2ND QUESTION
-   John has got a Mathematical assignment where he has to find mean of a given set of 3 integers. Write a Java program solution to help John .
-import java.io.*;
-import java.util.*;
 
-public class Solution 
-{
-   public static void main(String[] args)
-   {
-      Scanner sc=new Scanner(System.in);
-      double a=sc.nextDouble();
-      double b=sc.nextDouble();
-      double c=sc.nextDouble();
-      double d=((a+b+c)/3);
-     System.out.printf("%.1f",d-0.1);
-  }
-}
