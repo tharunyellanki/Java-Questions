b3bfcde1583ad351062a1f80c66fc38abe18a69c
YELLANKI THARUN <121236907+tharunyellanki@users.noreply.github.com>
2023-03-03 22:24:05 +0530

    Update and rename CSE 310 WEEK2-DAY 1 QUESTION-1 to CSE 310 WEEK2-DAY1-Q1.java

--- CSE 310 WEEK2-DAY1-Q1.java
+++ CSE 310 WEEK2-DAY1-Q1.java
@@ -1,9 +1,9 @@
-QUESTION-1
+/*QUESTION-1
 
 Prof. Decosta has designed multiple sets of the question paper of CSE310: Programming in Java subject. He has assigned a separate character code (A-Z) to each set of 
 question paper but mentioned the paper codes using binary numbers. He needs a program which can accept the binary paper code and convert it in to character code so that 
 he can easily identify the paper code during evaluation. If the paper code entered is not having 8 bits (0 or 1) or does not corresponds to A-Z then INVALID PAPER CODE 
-must be printed.
+must be printed.*/
 
 
 import java.io.*; 
