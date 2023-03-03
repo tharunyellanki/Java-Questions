4b567fa9c34489a5ea67f7aff7baf7779a258c98
YELLANKI THARUN <121236907+tharunyellanki@users.noreply.github.com>
2023-03-03 22:29:58 +0530

    Update and rename Create a method named ‘factorial’ in your program that will display the factorial of any given number. factorial of an integer is the product of numbers ranging from 1 to N to CSE 310 WEEK5-DAY4-Q1.java

--- CSE 310 WEEK5-DAY4-Q1.java
+++ CSE 310 WEEK5-DAY4-Q1.java
@@ -1,3 +1,7 @@
+/*Create a method named ‘factorial’ in your program that will display the factorial of any given number. factorial of an integer is the product of numbers ranging from 
+1 to N*/
+
+
 import java.util.Scanner;
 public class solution 
 {
