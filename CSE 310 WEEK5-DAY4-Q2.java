8649673dddf0d9c2c615acc395fad252ac8fe3be
YELLANKI THARUN <121236907+tharunyellanki@users.noreply.github.com>
2023-03-03 22:30:57 +0530

    Update and rename WAP to accpet one array of stings and integers.and count the number of stings and integers present in the array. Example Input: First line Accept size of array second line accept all elements like Array = {"Raj", "77", "101", "99", "Jio"} to CSE 310 WEEK5-DAY4-Q2.java

--- CSE 310 WEEK5-DAY4-Q2.java
+++ CSE 310 WEEK5-DAY4-Q2.java
@@ -1,3 +1,6 @@
+/*WAP to accpet one array of stings and integers.and count the number of stings and integers present in the array. Example Input: First line Accept size of array second 
+line accept all elements like Array = {"Raj", "77", "101", "99", "Jio"}*/
+
 import java.util.Scanner;
 
 public class Solution 
